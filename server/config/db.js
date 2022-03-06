55074ec41cfffd66b3796f26680e27632f9e5eb3
Silyan99 <bsilyan99@gmail.com>
2022-03-05 20:19:18 -0500

    Internal Documentation

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,9 @@
+/**
+ * Name: Bharat Silyan
+ * Student Id: 301147070
+ * File Name: db.js
+ */
+
 module.exports = {
   //local MongoDB deployment ->
   "URI": "mongodb+srv://Silyan99:bharat99-github@cluster0.qr7sk.mongodb.net/Midterm229?retryWrites=true&w=majority"
