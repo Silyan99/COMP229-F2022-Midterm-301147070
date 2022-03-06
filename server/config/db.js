f80e44f7efff79d356e1a60327f36ef01b666b8d
Silyan99 <bsilyan99@gmail.com>
2022-03-05 19:52:28 -0500

    Database updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://Silyan99:bharat99-github@cluster0.qr7sk.mongodb.net/Movies_Store?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://Silyan99:bharat99-github@cluster0.qr7sk.mongodb.net/midterm229?retryWrites=true&w=majority"
   /*ADD your cloud URL Here, database Name should be midterm229, and collection name should be the one in the model*/
 };
